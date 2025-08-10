c3c2ad976fa710c4d11dd9d466ffc21295d0054f
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2025-08-10 15:40:09 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,26 +1,32 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+
 author 'Azure(TheStoicBear)'
-description 'Azure Framework Jailing'
+description 'AZ-FW Jailer resource'
 version '1.0.0'
 
+-- Shared scripts
 shared_scripts {
     '@ox_lib/init.lua',
     'config.lua'
 }
 
+-- Server-side scripts
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'source/server.lua'
 }
 
+-- Client-side scripts
 client_scripts {
     'source/client.lua'
 }
 
+-- UI page
 ui_page 'UI/index.html'
 
+-- Files to be included
 files {
     'UI/index.html',
     'UI/config.js'
